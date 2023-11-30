79683365d509fce8e9e29c5887834200ac4507f5
ujin999 <epahg789@gmail.com>
2023-11-30 23:22:40 +0900

    #32 - security end

--- src/main/java/com/boardproject/projectboard/dto/security/BoardPrincipal.java
+++ src/main/java/com/boardproject/projectboard/dto/security/BoardPrincipal.java
@@ -22,7 +22,8 @@ public record BoardPrincipal(
     public static BoardPrincipal of(String username, String password, String email, String nickname, String memo) {
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
 
-        return new BoardPrincipal(username,
+        return new BoardPrincipal(
+                username,
                 password,
                 roleTypes.stream()
                         .map(RoleType::getName)
@@ -54,10 +55,9 @@ public record BoardPrincipal(
         );
     }
 
-    @Override public String getPassword() { return username; }
-    @Override public String getUsername() { return password; }
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() { return authorities; }
+    @Override public String getUsername() { return username; }
+    @Override public String getPassword() { return password; }
+    @Override public Collection<? extends GrantedAuthority> getAuthorities() { return authorities; }
 
     @Override public boolean isAccountNonExpired() { return true; }
     @Override public boolean isAccountNonLocked() { return true; }
@@ -67,8 +67,7 @@ public record BoardPrincipal(
     public enum RoleType {
         USER("ROLE_USER");
 
-        @Getter
-        private final String name;
+        @Getter private final String name;
 
         RoleType(String name) {
             this.name = name;
